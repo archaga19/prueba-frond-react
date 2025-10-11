0eae933c4f088e648c0a8960aac67357e420da39
Michael Archaga <michael.archaga@gmail.com>
2025-10-11 10:13:50 -0600

    botón con alerta funcional

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const mostrarAlerta = () => {
+    alert('¡Mi app está funcionando');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Test de proyecto
+          ¡Test de proyecto! 
         </h1>
         <p>
-          estamos en versel!!!
+         desplegada en Vercel!!! 
         </p>
+        
+        {/* BOTÓN NUEVO */}
+        <button 
+          className="boton-alerta"
+          onClick={mostrarAlerta}
+          style={{
+            padding: '12px 24px',
+            fontSize: '18px',
+            backgroundColor: '#61dafb',
+            color: '#282c34',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+            marginTop: '20px',
+            fontWeight: 'bold'
+          }}
+        >
+          funciona???
+        </button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
